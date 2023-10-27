47404a0185e099177a6721bded69400b653aaecb
Mostafa Akrami <mostafa@lunarapps.uk>
2023-10-27 14:47:41 +0100

    Complete OnboardingWindow UI
    The UI for this page is now completed.

--- OnboardingWindow.swift
+++ OnboardingWindow.swift
@@ -21,20 +21,180 @@ struct OnboardingWindow: View {
     
     var body: some View {
         VStack {
+            
+            /* Information
+             This part of the UI contains elements that describe to the user what the window's purpose is*/
+            
+            Text("Welcome to PassGuard")
+                .font(.system(size:20))
+                .bold()
+                .padding(.vertical, 1)
+            
+            Text("Before you get started, there are a few things you need to fill out first. Ensure all information entered is correct, as it cannot be changed later")
+                .font(.system(size:15))
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 17.5)
+                .padding(.horizontal, 20)
+            
+            /* Inputs
+             This part of the UI contains elements that allow the user to input personal information*/
+            
+            // Inputs - Name
+            
+            Text("Name")
+                .font(.system(size: 16))
+                .bold()
+                .padding(.top, 1)
+                .padding(.horizontal, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            TextField("Enter your full name", text: $fullName)
+                .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 47, idealHeight: 47, maxHeight: 47, alignment: .center)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal, 14)
+                .disableAutocorrection(true)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(inputBoxColor)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.teal, lineWidth: 3.5)
+                )
+                .padding(.horizontal, 20)
+                .padding(.bottom, 15)
+            
+            // Inputs - Master Password
+            
+            Text("Master Password")
+                .font(.system(size: 16))
+                .bold()
+                .padding(.top, 1)
+                .padding(.horizontal, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            SecureField("Create a strong Master Password", text: $masterPassword)
+                .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 47, idealHeight: 47, maxHeight: 47, alignment: .center)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal, 14)
+                .disableAutocorrection(true)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(inputBoxColor)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.teal, lineWidth: 3.5)
+                )
+                .padding(.horizontal, 20)
+                .padding(.bottom, 0)
+                .onChange(of: masterPassword) { masterPassword in
+                    strengthColour = updateStrength(masterPassword)
+                }
+            
+            // Inputs - Strength Meter
+            
+            HStack {
+                Text("Strength")
+                    .foregroundColor(.white)
+                    .padding(.vertical, 2)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 6.5)
                 
-                /* Information
-                 This part of the UI contains elements that describe to the user what the window's purpose is*/
+                Rectangle()
+                    .frame(height: 12)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .foregroundColor(strengthColour)
+                    .cornerRadius(7.5)
+                    .padding(.vertical, 2)
+                    .padding(.trailing, 20)
+            }
+            
+            // Inputs - Master Password Confirmation
+            
+            SecureField("Confirm Master Password", text: $confirmMasterPassword)
+                .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 47, idealHeight: 47, maxHeight: 47, alignment: .center)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal, 14)
+                .disableAutocorrection(true)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(inputBoxColor)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                    .stroke(passwordsMatch() ? Color.teal : Color.red, lineWidth: 3.5)
+                )
+                .padding(.horizontal, 20)
+                .padding(.top, 1)
+                .padding(.bottom, 5)
+            
+            // Inputs - Error Message
+            
+            if !passwordsMatch() {
+                Text("Passwords don't match. Try again.")
+                    .font(.system(size: 13))
+                    .padding(.top, 0)
+                    .padding(.bottom, 1)
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.red)
+            }
+            
+            // Inputs - Disclaimer Text
+            
+            Text("\nYour Master Password protects all objects stored in your manager. If you forget this, your data cannot be recovered.")
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 20)
+                .padding(.horizontal, 20)
+            
+            // Inputs - Confirm Registration Button
+            
+            Button("Create Account") {
                 
-                Text("Welcome to PassGuard")
-                    .font(.system(size:20))
-                    .bold()
-                    .padding(.vertical, 1)
-                
-                Text("Before you get started, there are a few things you need to fill out first. Ensure all information entered is correct, as it cannot be changed later")
-                
+            }
+                .buttonStyle(.plain)
+                .padding(.horizontal, 85)
+                .padding(.vertical, 8)
+                .background(Color.teal)
+                .foregroundColor(.black)
+                .bold()
+                .cornerRadius(30)
+                .frame(width: 50000)
+        }
+        .padding()
+    }
+    
+    /* Local Algorithms
+     This part of the application contains algorithms that control the functions of just this window*/
+    
+    // Local Algorithms - Strength Meter Logic
+    
+    func updateStrength(_ masterPassword: String) -> Color {
+        let score = PasswordComplexity(masterPassword)
+        
+        switch score {
+        case 1:
+            return Color.red
+        case 2:
+            return Color.orange
+        case 3:
+            return Color.yellow
+        case 4:
+            return Color.green
+        case 5:
+            return Color.blue
+        default:
+            return Color.gray
         }
     }
     
+    // Local Algorithms - Passwords Matching Logic
+    
+    func passwordsMatch() -> Bool {
+        return masterPassword == confirmMasterPassword
+    }
 }
 
 /* Launch View
