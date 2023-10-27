5e1ab20c4ac014cfb85f5dcb473f60af667168a0
Mostafa Akrami <mostafa@lunarapps.uk>
2023-10-27 15:14:59 +0100

    Update Algorithms.swift
    Moved a function from OnboardingWindow to become global

--- Algorithms.swift
+++ Algorithms.swift
@@ -9,28 +9,51 @@ When a user enters a password into PassGuard’s database, this algorithm should
 func PasswordComplexity(_ password: String) -> Int {
     var complexityScore = 0
     
-    // Checks Password Length
+    // Checks the length of the password, with a minimum of 8, and extra points for a password greater than 13 characters long.
     if password.count >= 14 {
         complexityScore += 2
     } else if password.count >= 8 {
         complexityScore += 1
     }
     
-    // Checks if Password has upper and lowercase
+    // Checks if the password has upper and lowercase characters using regexp.
     if password.range(of: #"(?=.*[A-Z])(?=.*[a-z])"#, options: .regularExpression) != nil {
         complexityScore += 1
     }
     
-    // Checks if Password has numbers
+    // Checks if the password has numbers using regexp.
     if password.range(of: #".*[0-9].*"#, options: .regularExpression) != nil {
         complexityScore += 1
     }
     
-    // Checks if Password has special characters
+    // Checks if the password has special characters using regexp.
     if password.range(of: #".*[^A-Za-z0-9].*"#, options: .regularExpression) != nil {
         complexityScore += 1
     }
     
-    // Gives score out of 5
+    // Returnes a score out of five.
     return complexityScore
+}
+
+/* Password Complexity Colour
+A sub-algorithm of the PasswordComplextiy() feature, which assigns colours instead of a numeric score.*/
+
+func PasswordComplexityColour(_ masterPassword: String) -> Color {
+    let score = PasswordComplexity(masterPassword)
+    
+    // For each case, a colour is assigned from scores 1 to 5, one being the weakest and logically associated with red.
+    switch score {
+    case 1:
+        return Color.red
+    case 2:
+        return Color.orange
+    case 3:
+        return Color.yellow
+    case 4:
+        return Color.green
+    case 5:
+        return Color.blue
+    default:
+        return Color.gray
+    }
 }
