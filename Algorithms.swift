f03df9b2e555e59ef0c5c4b04dbe0b1db6c3bd1a
Mostafa Akrami <mostafa@lunarapps.uk>
2023-10-27 15:15:45 +0100

    Changed a function to global
    Moved an algorithm to become global

--- Algorithms.swift
+++ Algorithms.swift
@@ -2,58 +2,190 @@
 These are required for PassGuard to function correctly and are not programmed by me. They are external tools used to make development easier. */
 
 import SwiftUI
+import SwiftData
 
-/* Password Complexity
-When a user enters a password into PassGuard’s database, this algorithm should run to check for complexity and tag potentially weaker credentials that should be replaced.*/
+struct OnboardingWindow: View {
+    
+    /* Variables
+     These are values which change in this specfic view before any major changes are pushed to the backend. The @State makes the variable non-static in SwiftUI*/
 
-func PasswordComplexity(_ password: String) -> Int {
-    var complexityScore = 0
+    @State private var fullName = ""
+    @State private var masterPassword = ""
+    @State private var confirmMasterPassword = ""
+    @State private var registrationSuccessful = false
+    @State var strengthColour = Color.gray
+    let inputBoxColor = Color(red: 72 / 255, green: 74 / 255, blue: 78 / 255)
     
-    // Checks the length of the password, with a minimum of 8, and extra points for a password greater than 13 characters long.
-    if password.count >= 14 {
-        complexityScore += 2
-    } else if password.count >= 8 {
-        complexityScore += 1
+    /* Window View
+    This is the structure of the window that appears when the OnboardingWindow view is called*/
+    
+    var body: some View {
+        VStack {
+            
+            /* Information
+             This part of the UI contains elements that describe to the user what the window's purpose is*/
+            
+            Text("Welcome to PassGuard")
+                .font(.system(size:20))
+                .bold()
+                .padding(.vertical, 1)
+            
+            Text("Before you get started, there are a few things you need to fill out first. Ensure all information entered is correct, as it cannot be changed later")
+                .font(.system(size:15))
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 17.5)
+                .padding(.horizontal, 20)
+            
+            /* Inputs
+             This part of the UI contains elements that allow the user to input personal information*/
+            
+            // Inputs - Name
+            
+            Text("Name")
+                .font(.system(size: 16))
+                .bold()
+                .padding(.top, 1)
+                .padding(.horizontal, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            TextField("Enter your full name", text: $fullName)
+                .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 47, idealHeight: 47, maxHeight: 47, alignment: .center)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal, 14)
+                .disableAutocorrection(true)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(inputBoxColor)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.teal, lineWidth: 3.5)
+                )
+                .padding(.horizontal, 20)
+                .padding(.bottom, 15)
+            
+            // Inputs - Master Password
+            
+            Text("Master Password")
+                .font(.system(size: 16))
+                .bold()
+                .padding(.top, 1)
+                .padding(.horizontal, 20)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            SecureField("Create a strong Master Password", text: $masterPassword)
+                .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 47, idealHeight: 47, maxHeight: 47, alignment: .center)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal, 14)
+                .disableAutocorrection(true)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(inputBoxColor)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.teal, lineWidth: 3.5)
+                )
+                .padding(.horizontal, 20)
+                .padding(.bottom, 0)
+                .onChange(of: masterPassword) { masterPassword in
+                    strengthColour = PasswordComplexityColour(masterPassword)
+                }
+            
+            // Inputs - Strength Meter
+            
+            HStack {
+                Text("Strength")
+                    .foregroundColor(.white)
+                    .padding(.vertical, 2)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 6.5)
+                
+                Rectangle()
+                    .frame(height: 12)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .foregroundColor(strengthColour)
+                    .cornerRadius(7.5)
+                    .padding(.vertical, 2)
+                    .padding(.trailing, 20)
+            }
+            
+            // Inputs - Master Password Confirmation
+            
+            SecureField("Confirm Master Password", text: $confirmMasterPassword)
+                .frame(minWidth: 0, idealWidth: .infinity, maxWidth: .infinity, minHeight: 47, idealHeight: 47, maxHeight: 47, alignment: .center)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal, 14)
+                .disableAutocorrection(true)
+                .background(
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(inputBoxColor)
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                    .stroke(passwordsMatch() ? Color.teal : Color.red, lineWidth: 3.5)
+                )
+                .padding(.horizontal, 20)
+                .padding(.top, 1)
+                .padding(.bottom, 5)
+            
+            // Inputs - Error Message
+            
+            if !passwordsMatch() {
+                Text("Passwords don't match. Try again.")
+                    .font(.system(size: 13))
+                    .padding(.top, 0)
+                    .padding(.bottom, 1)
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.red)
+            }
+            
+            // Inputs - Disclaimer Text
+            
+            Text("\nYour Master Password protects all objects stored in your manager. If you forget this, your data cannot be recovered.")
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 20)
+                .padding(.horizontal, 20)
+            
+            // Inputs - Confirm Registration Button
+            
+            Button("Create Account") {
+                
+            }
+                .buttonStyle(.plain)
+                .padding(.horizontal, 85)
+                .padding(.vertical, 8)
+                .background(Color.teal)
+                .foregroundColor(.black)
+                .bold()
+                .cornerRadius(30)
+                .frame(width: 50000)
+        }
+        .padding()
     }
     
-    // Checks if the password has upper and lowercase characters using regexp.
-    if password.range(of: #"(?=.*[A-Z])(?=.*[a-z])"#, options: .regularExpression) != nil {
-        complexityScore += 1
-    }
+    /* Local Algorithms
+     This part of the application contains algorithms that control the functions of just this window*/
     
-    // Checks if the password has numbers using regexp.
-    if password.range(of: #".*[0-9].*"#, options: .regularExpression) != nil {
-        complexityScore += 1
-    }
+    // Local Algorithms - Passwords Matching Logic
     
-    // Checks if the password has special characters using regexp.
-    if password.range(of: #".*[^A-Za-z0-9].*"#, options: .regularExpression) != nil {
-        complexityScore += 1
+    func passwordsMatch() -> Bool {
+        return masterPassword == confirmMasterPassword
     }
-    
-    // Returnes a score out of five.
-    return complexityScore
 }
 
-/* Password Complexity Colour
-A sub-algorithm of the PasswordComplextiy() feature, which assigns colours instead of a numeric score.*/
+/* Launch View
+This is the entry point for PassGuard. If there has not been a registration form completed, this will be the first window to open.*/
 
-func PasswordComplexityColour(_ masterPassword: String) -> Color {
-    let score = PasswordComplexity(masterPassword)
-    
-    // For each case, a colour is assigned from scores 1 to 5, one being the weakest and logically associated with red.
-    switch score {
-    case 1:
-        return Color.red
-    case 2:
-        return Color.orange
-    case 3:
-        return Color.yellow
-    case 4:
-        return Color.green
-    case 5:
-        return Color.blue
-    default:
-        return Color.gray
+@main
+struct OnboardingWidnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingWindow()
+                .frame(minWidth: 450, minHeight: 600)
+                .background(Color(red: 23/255, green: 23/255, blue: 23/255))
+        }
     }
 }
